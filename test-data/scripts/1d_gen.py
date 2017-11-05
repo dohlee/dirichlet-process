38f1dc7686593a55a0a39b022afc66445c06e128
dohlee <apap950419@gmail.com>
2017-11-05 22:13:31 +0900

    Make data centered to 0

--- test-data/scripts/1d_gen.py
+++ test-data/scripts/1d_gen.py
@@ -5,7 +5,7 @@ import seaborn as sns; sns.set()
 sns.set_style('white')
 
 ALPHA = 10
-STDDEV = 0.3
+STDDEV = 0.4
 NUM_DATA = 100
 COLORS = ['red', 'green', 'blue', 'black', 'purple']
 
@@ -16,12 +16,12 @@ def generate_test_data(numData, numCluster):
 	# Mixing coefficient.
 	pi = np.random.dirichlet([ALPHA] * numCluster)
 	# Means of clusters.
-	mus = np.linspace(start=0, stop=numCluster, num=numCluster) + np.random.normal(loc=0, scale=0.1, size=numCluster)
+	mus = np.linspace(start=0, stop=1, num=numCluster) + np.random.normal(loc=0, scale=0.01, size=numCluster)
 	# Generate and return data.
 	data, labels = [], []
 	for _ in range(numData):
 		mu = np.random.choice(mus, p=pi)
-		d = np.random.normal(loc=mu, scale=STDDEV)
+		d = np.random.normal(loc=mu, scale=STDDEV / numCluster)
 		data.append(d)
 		labels.append(np.where(mus == mu)[0])
 
